e0aebc0078a4a37c4ba2034c908aa27a4e92673f
rpashev <56789393+rpashev@users.noreply.github.com>
2021-04-28 17:48:47 +0300

    connecting to mongo db

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const routes = require('./routes/routes')
+const routes = require("./routes/routes");
 
 const app = express();
 
 app.use(routes);
 
-app.listen(5000, console.log("listening on port 5000...."))
+mongoose
+  .connect("mongodb+srv://rosko:Rossen91kz@cluster0.qhqg3.mongodb.net/movie-app?retryWrites=true&w=majority", { useNewUrlParser: true,useUnifiedTopology: true }, )
+  .then(() => app.listen(5000, console.log("listening on port 5000....")))
+  .catch((err) => console.log(err));
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,27 @@
 const express = require("express");
 
+const createError = require("http-errors");
+
 const router = express.Router();
 
 const DUMMY_DATA = [
-  { id: 1, title: "Batman" },
-  { id: 2, title: "The Lord Of The Rings" },
-  { id: 3, title: "The Matrix" },
+  { id: "m1", title: "Batman" },
+  { id: "m2", title: "The Lord Of The Rings" },
+  { id: "m3", title: "The Matrix" },
 ];
 
-router.get("/:movieId", (req, res, next) => {
-  const id = req.params.movieId;
-  const movie = DUMMY_DATA.find((m) => m.id === +id);
-  res.json({ movie });
-});
+router.get('/:movieId', (req, res, next) => {
+    const movieId = req.params.movieId; 
+  
+    const movie = DUMMY_DATA.find(m => {
+      return m.id === movieId;
+    });
+  
+    if (!movie) {
+      throw new createError(401, 'Could not find a movie for the provided id.');
+    }
+  
+    res.json({ movie }); 
+  });
 
 module.exports = router;
