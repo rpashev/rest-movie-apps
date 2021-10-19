debf97145247fc9098b40271757212d52dca9e25
rpashev <rossen1991@gmail.com>
2021-10-19 17:01:55 +0300

    added validations for signing up and hashed password

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,14 +1,71 @@
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  
+  const { username, email, password, repeatPassword } = req.body;
+
+  if (!username) {
+    const error = new HttpError("Username is required!", 400);
+    return next(error);
+  }
+  if (repeatPassword !== password || password.length < 6) {
+    const error = new HttpError(
+      "Passwords must match and be at least 6 symbols!",
+      400
+    );
+    return next(error);
+  }
+  //validate email
+  let regex = /\S+@\S+\.\S+/;
+  if (regex.test(email) === false || email === "") {
+    const error = new HttpError("Invalid email!", 400);
+    return next(error);
+  }
+
+  let existingUser;
+  //check if email is used
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Could not sign up, please try again!", 500);
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError(
+      "User with such email already exists! Please login instead!",
+      422
+    );
+    return next(error);
+  }
+  //check if username is taken
+  try {
+    existingUser = await User.findOne({ username: username });
+  } catch (err) {
+    const error = new HttpError("Could not sign up, please try again!", 500);
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError(
+      "This username is taken, please choose another one!",
+      422
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not register, please try again!", 500);
+    return next(error);
+  }
   const user = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
+
   try {
     await user.save();
     res.send("Success");
