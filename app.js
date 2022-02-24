e3d8a4e75133fa06ddffafc8576b4f075fad4c8b
rpashev <rossen1991@gmail.com>
2022-02-24 17:55:40 +0200

    test prod

--- app.js
+++ app.js
@@ -13,24 +13,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     // credentials: true,
-//     // origin: "http://localhost:3000",
-//     // origin: true,
-//   })
-// );
+app.use(
+  cors({
+    // credentials: true,
+    // origin: "http://localhost:3000",
+    // origin: true,
+  })
+);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
+app.options('*', cors())
 
 app.use("/auth/", authRoutes);
 app.use(publicRoutes);
