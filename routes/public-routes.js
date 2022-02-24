daf25b0e06fecd08970730d9ac20e7754221fed0
rpashev <rossen1991@gmail.com>
2022-02-24 18:25:38 +0200

    test

--- routes/public-routes.js
+++ routes/public-routes.js
@@ -3,9 +3,9 @@ import publicControllers from "../controllers/public-controller.js";
 import attachUserId from "../middleware/attachUserId.js";
 
 const router = express.Router();
-
+router.get("/", (req, res) => res.send("HELLOOOO"));
 router.get("/movies/:movieId", attachUserId, publicControllers.getMovie); //testing
-router.get("/public-library",attachUserId, publicControllers.getPublicList);
+router.get("/public-library", attachUserId, publicControllers.getPublicList);
 
 // module.exports = router;
 export default router;
