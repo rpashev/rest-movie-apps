f3eb95de043a0667ef9967424759118e177124ca
rpashev <rossen1991@gmail.com>
2022-03-21 16:44:21 +0200

    connection string revised

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ import mongoose from "mongoose";
 import app from "./app.js";
 
 mongoose
-  .connect(
-    `mongodb://${process.env.USER_MONGO}:${process.env.USER_MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}-shard-00-00.cpss2.mongodb.net:27017,${process.env.MONGO_CLUSTER}-shard-00-01.cpss2.mongodb.net:27017,${process.env.MONGO_CLUSTER}-shard-00-02.cpss2.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-dg8hi2-shard-0&authSource=admin&retryWrites=true&w=majority`
-  )
+  .connect(process.env.DB_CONNECTION)
   .then(() =>
     app.listen(
       process.env.PORT || 5000,
