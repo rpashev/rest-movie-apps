73e1f73d0acbc949135ed70feb6e89c3c281912c
rpashev <rossen1991@gmail.com>
2022-03-29 11:39:18 +0300

    limit data size in body

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 app.use(
   cors({
